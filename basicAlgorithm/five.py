bb02c8ee8027d4d19eaeea7fb9d2444976a02641
禾几 <72876987+XingchaoNie@users.noreply.github.com>
2021-01-13 16:09:09 +0800

    Add files via upload

--- basicAlgorithm/five.py
+++ basicAlgorithm/five.py
@@ -12,5 +12,13 @@ if __name__ == '__main__':
                 grad = gra[:-1].split('\t')
                 data[int(grad[0]) - 1, int(grad[1]) - 1] += float(grad[2])
     data = data/5
+    with open('finalGrade.txt', 'w') as final:
+        for i in range(np.shape(data)[0]):
+            for j in range(np.shape(data)[1]):
+                final.write(str(data[i, j]))
+                final.write('\t')
+            final.write('\n')
+
+
 
 
